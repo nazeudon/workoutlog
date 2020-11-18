055872015a4e038fcecd67b3f390c1ac9df1990d
nazeudon <nazeudon48112@icloud.com>
2020-11-18 11:21:50 +0900

    progress

--- react_redux/src/features/event/NewEvent.tsx
+++ react_redux/src/features/event/NewEvent.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../app/store";
 import { TextField, Button, IconButton } from "@material-ui/core";
 import styles from "./NewEvent.module.css";
 import {
-  fetchAsyncGetEvents,
   fetchAsyncNewEvent,
   fetchPostEnd,
   fetchPostStart,
